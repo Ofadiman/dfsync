996ab5659b7431d11f680ff6b46058c68086d873
ofadiman <ofadiman@gmail.com>
2024-02-18 14:06:16 +0100

    feature: setup root command

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:     "dfsync",
+	Short:   "Synchronize dot files",
+	Example: "dfsync --source ./src --destination ~/",
+	Long:    `Dot files sync is a tool that allows you to painlessly synchronize dot files across multiple environments.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+}
 
 func main() {
-	fmt.Println("works")
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
