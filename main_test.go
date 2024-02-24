c8ec5a12948634568faba854d5e0ba5675c58525
ofadiman <ofadiman@gmail.com>
2024-02-24 21:22:05 +0100

    chore: sort snapshots in deterministic order

--- main_test.go
+++ main_test.go
@@ -11,7 +11,7 @@ func TestMain(m *testing.M) {
 	v := m.Run()
 
 	// Cleanup obsolete snapshot tests after all tests were run.
-	snaps.Clean(m)
+	snaps.Clean(m, snaps.CleanOpts{Sort: true})
 
 	os.Exit(v)
 }
