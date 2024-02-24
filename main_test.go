fa4f0931b059ce540b2048448e57b11e5cc12140
ofadiman <ofadiman@gmail.com>
2024-02-24 21:20:56 +0100

    chore: automatically remove obsolete snapshot tests

---
+++ main_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/gkampitakis/go-snaps/snaps"
+)
+
+func TestMain(m *testing.M) {
+	v := m.Run()
+
+	// Cleanup obsolete snapshot tests after all tests were run.
+	snaps.Clean(m)
+
+	os.Exit(v)
+}
